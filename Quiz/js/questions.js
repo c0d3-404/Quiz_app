e55efbb4c9fa80b6025598e3e74180ef160871dd
c0d3-404 <98340031+c0d3-404@users.noreply.github.com>
2023-11-28 17:03:29 +0000

    updated questions and fixed bug

--- Quiz/js/questions.js
+++ Quiz/js/questions.js
@@ -22,10 +22,11 @@ let questions = [
         numb: 3,
         question:
             "What will be the datatype of the var in the below code snippet?" +
-            `<i><<pre><code class="python">var = 10
-        print(type(var))
-        var="Hello"
-        print(type(var))`,
+            `<i><<pre><code class="python">
+            var = 10
+            print(type(var))
+            var="Hello"
+            print(type(var))`,
         answer: "int and str",
         options: ["str and int", "int and int", "str and str", "int and str"],
     },
@@ -41,10 +42,11 @@ let questions = [
         numb: 5,
         question:
             "What will be the output of the following code snippet?" +
-            `<i><<pre><code class="python">a = [1, 2, 3]
-        a = tuple(a)
-        a[0] = 2
-        print(a)`,
+            `<i><<pre><code class="python">
+            a = [1, 2, 3]
+            a = tuple(a)
+            a[0] = 2
+            print(a)`,
         answer: "Error",
         options: ["[2,2,3]", "(2,2,3)", "(1,2,3)", "Error"],
     },
@@ -68,11 +70,12 @@ let questions = [
         numb: 7,
         question:
             "What will be the output of the following code snippet?" +
-            `<i><<pre><code class="python">a = [1, 2, 3, 4, 5]
-        sum = 0
-        for ele in a:
-            sum += ele 
-        print(sum)`,
+            `<i><<pre><code class="python">
+            a = [1, 2, 3, 4, 5]
+            sum = 0
+            for ele in a:
+                sum += ele 
+            print(sum)`,
         answer: "float and int",
         options: [
             "float and int",
@@ -86,11 +89,12 @@ let questions = [
         numb: 8,
         question:
             "What will be the output of the following code snippet?" +
-            `<i><<pre><code class="python">a = 3
-        b = 1 
-        print(a, b)
-        a, b = b, a 
-        print(a, b)`,
+            `<i><<pre><code class="python">
+            a= 3
+            b = 1 
+            print(a, b)
+            a, b = b, a 
+            print(a, b)`,
         answer: "3 1   1 3",
         options: ["3 1   1 3", "1 3   3 1", "3 1   1 3", "1 3   3 1"],
     },
@@ -99,8 +103,9 @@ let questions = [
         numb: 9,
         question:
             "What will be the output of the following code snippet?" +
-            `<i><<pre><code class="python">a = [1, 2]
-        print(a * 3)`,
+            `<i><<pre><code class="python">
+            a = [1, 2]
+            print(a * 3)`,
         answer: "[1,2,1,2,1,2]",
         options: ["Error", "[1,2]", "[3,6]", "[1,2,1,2,1,2]"],
     },
@@ -117,9 +122,10 @@ let questions = [
         numb: 11,
         question:
             "What will be the output of the following code snippet?" +
-            `<i><<pre><code class="python">example = ["Sunday", "Monday", "Tuesday", "Wednesday"];
-        del example[2]
-        print(example)`,
+            `<i><<pre><code class="python">
+            example = ["Sunday", "Monday", "Tuesday", "Wednesday"];
+            del example[2]
+            print(example)`,
         answer: "['Sunday','Monday', 'Wednesday']",
         options: [
             "['Sunday', 'Monday', 'Tuesday', 'Wednesday']",
@@ -146,9 +152,10 @@ let questions = [
         numb: 13,
         question:
             "What will be the type of the variable sorted_numbers in the below code snippet?" +
-            `<i><<pre><code class="python">numbers = (4, 7, 19, 2, 89, 45, 72, 22)
-        sorted_numbers = sorted(numbers)
-        print(sorted_numbers)`,
+            `<i><<pre><code class="python">
+            numbers = (4, 7, 19, 2, 89, 45, 72, 22)
+            sorted_numbers = sorted(numbers)
+            print(sorted_numbers)`,
         answer: "List",
         options: ["List", "Tuple", "String", "Int"],
     },
@@ -157,19 +164,20 @@ let questions = [
         numb: 14,
         question:
             "What will be the output of the following code snippet?" +
-            `<i><<pre><code class="python">def thrive(n):
-            if n % 15 == 0:
-                print("thrive", end = “ ”)
-            elif n % 3 != 0 and n % 5 != 0:
-                print("neither", end = “ ”)
-            elif n % 3 == 0:
-                print("three", end = “ ”)
-            elif n % 5 == 0:
-                print("five", end = “ ”)
-        thrive(35)
-        thrive(56)
-        thrive(15)
-        thrive(39)`,
+            `<i><<pre><code class="python">
+            def thrive(n):
+                if n % 15 == 0:
+                    print("thrive", end = “ ”)
+                elif n % 3 != 0 and n % 5 != 0:
+                    print("neither", end = “ ”)
+                elif n % 3 == 0:
+                    print("three", end = “ ”)
+                elif n % 5 == 0:
+                    print("five", end = “ ”)
+            thrive(35)
+            thrive(56)
+            thrive(15)
+            thrive(39)`,
         answer: "five neither thrive three",
         options: [
             "five neither thrive three",
@@ -183,11 +191,12 @@ let questions = [
         numb: 15,
         question:
             "What will be the output of the following code snippet?" +
-            `<i><<pre><code class="python">numbers = (4, 7, 19, 2, 89, 45, 72, 22)
-        sorted_numbers = sorted(numbers)
-        even = lambda a: a % 2 == 0
-        even_numbers = filter(even, sorted_numbers)
-        print(type(even_numbers))`,
+            `<i><<pre><code class="python">
+            numbers = (4, 7, 19, 2, 89, 45, 72, 22)
+            sorted_numbers = sorted(numbers)
+            even = lambda a: a % 2 == 0
+            even_numbers = filter(even, sorted_numbers)
+            print(type(even_numbers))`,
         answer: "filter",
         options: ["filter", "int", "list", "tuple"],
     },
@@ -196,10 +205,238 @@ let questions = [
         numb: 16,
         question:
             "What will be the output of the following code snippet?" +
-            `<i><<pre><code class="python">def check(a):
-            print("Even" if a % 2 == 0 else "Odd")
-        check(12)`,
+            `<i><<pre><code class="python">
+            def check(a):
+                print("Even" if a % 2 == 0 else "Odd")
+            check(12)`,
         answer: "Even",
         options: ["Even", "Odd", "Error", "None of the above"],
+    },
+    {
+        //q17
+        numb: 17,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            numbers = (4, 7, 19, 2, 89, 45, 72, 22)
+            sorted_numbers = sorted(numbers)
+            odd_numbers = [x for x in sorted_numbers if x % 2 != 0]
+            print(odd_numbers)`,
+        answer: "[7,19,45,89]",
+        options: [
+            "[7,19,45,89]",
+            "[2,4,22,72]",
+            "[4,7,19,2,89,45,72,22]",
+            "[2,4,7,19,22,45,72,89]",
+        ],
+    },
+    {
+        //q18
+        numb: 18,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            example = ["Sunday", "Monday", "Tuesday", "Wednesday"];
+            print(example[-3:-1])`,
+        answer: "['Monday','Tuesday']",
+        options: [
+            "['Monday','Tuesday']",
+            "['Sunday','Monday']",
+            "['Tuesday','Wednesday']",
+            "['Wednesday','Monday']",
+        ],
+    },
+    {
+        //q19
+        numb: 19,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            def is_even(number):
+                message =  f"{number} is an even number" if number % 2 == 0 else  f"{number} is an odd number"
+                return message
+            print(is_even(54))`,
+        answer: "54 is an even number",
+        options: [
+            "54 is an even number",
+            "54 is an odd number",
+            "number is an even number",
+            "number is an odd number",
+        ],
+    },
+    {
+        //q20
+        numb: 20,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            dict1 = {'first' : 'sunday', 'second' : 'monday'}
+            dict2 = {1: 3, 2: 4}
+            dict1.update(dict2)
+            print(dict1)`,
+        answer: "{'first': 'sunday', 'second': 'monday', 1: 3 :2: 4}",
+        options: [
+            "{'first': 'sunday', 'second': 'monday', 1: 3 :2: 4}",
+            "{'first': 'sunday', 'second': 'monday'}",
+            "{1: 3 :2: 4}",
+            "None of the above",
+        ],
+    },
+    {
+        //q21
+        numb: 21,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            s = {1, 2, 3, 3, 2, 4, 5, 5}
+            print(s)`,
+        answer: "{1, 2, 3, 4, 5}",
+        options: [
+            "{1, 2, 3, 3, 2, 4, 5, 5}",
+            "{1, 2, 3, 4, 5}",
+            "{1, 5}",
+            "None of the above",
+        ],
+    },
+    {
+        //q22
+        numb: 22,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            a = {'Hello':'World', 'First': 1}
+            b = {val: k for k , val in a.items()}
+            print(b)`,
+        answer: "{1, 2, 3, 4, 5,}",
+        options: [
+            "{'Hello':'World', 'First': 1}",
+            "{'World': 'Hello', 1: 'First}",
+            "All of the above",
+            "None of the above",
+        ],
+    },
+    {
+        //q23
+        numb: 23,
+        question:
+            "Which of the following functions converts date to corresponding time in Python?",
+        answer: "strptime()",
+        options: [
+            "strptime()",
+            "strftime()",
+            "All of the above",
+            "None of the above",
+        ],
+    },
+    {
+        //q24
+        numb: 24,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            word = "Python Programming"
+            n = len(word)
+            word1 = word.upper()
+            word2 = word.lower()
+            converted_word = ""
+            for i in range(n):
+                if i % 2 == 0:
+                    converted_word += word2[i]
+                else:
+                    converted_word += word1[i]
+            print(converted_word)`,
+        answer: "pYtHoN PrOgRaMmInG",
+        options: [
+            "pYtHoN PrOgRaMmInG",
+            "Python Progamming",
+            "python programing",
+            "PYTHON PROGRAMMING",
+        ],
+    },
+    {
+        //q25
+        numb: 25,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            a = "4, 5"
+            nums = a.split(',')
+            x, y = nums
+            int_prod = int(x) * int(y)
+            print(int_prod)`,
+        answer: "20",
+        options: ["20", "45", "54", "4,5"],
+    },
+    {
+        //q26
+        numb: 26,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            square = lambda x: x ** 2
+            a = []
+            for i in range(5):
+            a.append(square(i))
+            
+            print(a)`,
+        answer: "[0,1,4,9,16]",
+        options: [
+            "[0,1,4,9,16]",
+            "[1,4,9,16,25]",
+            "[0,1,2,3,4]",
+            "[1,2,3,4,5]",
+        ],
+    },
+    {
+        //q27
+        numb: 27,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            def tester(*argv):
+                for arg in argv:
+                    print(arg, end = ' ')
+            tester('Sunday', 'Monday', 'Tuesday', 'Wednesday')`,
+        answer: "Sunday Monday Tuesday Wednesday",
+        options: [
+            "Sunday",
+            "Wednesday",
+            "Sunday Monday Tuesday Wednesday",
+            "None of the above",
+        ],
+    },
+    {
+        //q28
+        numb: 28,
+        question:
+            "What datatype are the *args stored, when passed into a function?",
+        answer: "Tuple",
+        options: ["List", "Tuple", "Dictionary", "None of the above"],
+    },
+    {
+        //q29
+        numb: 29,
+        question:
+            "What will be the output of the following code snippet?" +
+            `<i><<pre><code class="python">
+            def tester(**kwargv):
+                for key, value in kwargs.items():
+                    print(key, value, end = " ")
+            tester(Sunday = 1, Monday = 2, Tuesday = 3, Wednesday = 4)`,
+        answer: "Sunday 1 Monday 2 Tuesday 3 Wednesday 4",
+        options: [
+            "Sunday 1 Monday 2 Tuesday 3 Wednesday 4",
+            "Sunday 1",
+            "Wednesday 4",
+            "None of the above",
+        ],
+    },
+    {
+        //q30
+        numb: 30,
+        question:
+            "What datatype are the *kwargs stored, when passed into a function?",
+        answer: "Dictionary",
+        options: ["List", "Tuple", "Dictionary", "None of the above"],
     },
 ];
